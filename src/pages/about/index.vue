d617d95d5b4964060387884e83f6d9fb8e9bda5c
宋青松 <suvlliansong@163.com>
2017-09-25 20:50:43 +0800

    'refactor'

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,33 +1,24 @@
 <template>
-	<div class="article-page">
-		<div class="article-list">
-			<article class="article">
-				<Header :article-head="article" />
-				<div class="article-content">
-					<div class="img-center">
-						<img class="img-big" :src="article.aImage">
-					</div>
-				</div>
-				<div class="article-content about" v-html="article.aContent"></div>
-			</article>
-		</div>
+	<div class="article-contianer about">
+		<section>
+			<h3 class="about-title">
+        <strong>关于我<span><font>/About Me</font></span></strong>
+			</h3>
+			<div>
+        <p>我是宋青松，毕业于长安大学网络工程专业。</p>
+				<p>专注但不局限于前端开发。</p>
+				<p>喜欢各种新鲜事物。喜欢旅行、音乐、写作和摄影。</p>
+			</div>
+		</section>
+		<section>
+			<h3 class="about-title">
+        <strong>社交信息<span><font>/Social Information</font></span></strong>
+			</h3>
+			<ul>
+        <li>Github：<a href="https://github.com/suvllian" title="Github" target="_blank">@Suvllian</a></li>
+        <li>Social：<a href="http://weibo.com/5377632866" title="微博：瓦尔登湖啊盘一棵松" target="_blank">新浪微博</a></li>
+        <li>Email：<a href="mailto:suvlliansong@163.com" title="邮箱" target="_blank">suvlliansong@163.com</a></li>
+			</ul>
+		</section>
 	</div>
 </template>
-
-<script>
-import Header from './components/header.vue'	
-import { mapActions, mapState} from 'vuex'
-import { GET_ABOUT_INFO, GET_ABOUT_INFO_FAILURE } from './../../vuex/type.js'
-export default{
-	components: { Header },
-	computed: mapState({ 
-		article: store => store.aboutInfo.article,
-	}),
-	methods: { 
-		...mapActions([GET_ABOUT_INFO]) 
-	},
-  created(){
-		this.GET_ABOUT_INFO();
-  }
-}
-</script>
